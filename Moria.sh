08392b3f92168281cee9f8a45f43c268121c467c
tiarafthzzhr <tiarafthzzhr@gmail.com>
2025-11-30 22:47:07 +0700

    Add static IP configuration for multiple interfaces

---
+++ Moria.sh
@@ -0,0 +1,15 @@
+auto eth0
+iface eth0 inet static
+    address 192.168.220.9
+    netmask 255.255.255.252
+    gateway 192.168.220.10
+
+auto eth1
+iface eth1 inet static
+    address 192.168.220.1
+    netmask 255.255.255.252
+
+auto eth2
+iface eth2 inet static
+    address 192.168.220.5
+    netmask 255.255.255.252
